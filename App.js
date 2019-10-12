893a3d800c6dd802b194044889131475ba29c8b8
Avani <avani@Apples-MacBook-Pro.local>
2019-10-13 01:24:31 +0530

    ft: conversion functions.

--- App.js
+++ App.js
@@ -13,13 +13,15 @@ export class App extends React.Component {
   }
 
   componentWillMount() {
-    let randonmStr = create64String();
-    const b64encoded = btoa(
-      randonmStr, // 4*ceil(n/3) so n = 6  i.e. 8 bytes = 64 bits
-    );
-    const binaryArr = base64js.toByteArray(b64encoded);
-    console.log('b64encoded====', b64encoded);
+    let randonmStr = create64String(); // w8TTelZzoWdP1otQIod3KoddYHRasaTBr5keryt1BVDCnhhnVWfImRl7g6Om4zZE
+    // const b64encoded = btoa( // dzhUVGVsWnpvV2RQMW90UUlvZDNLb2RkWUhSYXNhVEJyNWtlcnl0MUJWRENuaGhuVldmSW1SbDdnNk9tNHpaRQ==
+    //   'w8TTelZzoWdP1otQIod3KoddYHRasaTBr5keryt1BVDCnhhnVWfImRl7g6Om4zZE', // 4*ceil(n/3) so n = 6  i.e. 8 bytes = 64 bits
+    // );
+    const binaryArr = hexStringToByte('w8TTelZzoWdP1otQIod3KoddYHRasaTBr5keryt1BVDCnhhnVWfImRl7g6Om4zZE')
+  //base64js.toByteArray(b64encoded);
+    //console.log('b64encoded====', b64encoded);
     console.log('bytes====',binaryArr);
+    console.log('from byte array===', toHexString(binaryArr))
     Realm.open({
       schema: [{name: 'Dog', properties: {name: 'string'}}],
       encryptionKey: binaryArr,
@@ -49,3 +51,15 @@ function create64String(){
 										console.log('@@@@@@ random text ', text);
 										return text;
                   }
+function toHexString(byteArray) {
+      return Array.from(byteArray, function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+      }).join('')
+    }
+function hexStringToByte(str) {
+  let byteArray = [];
+  for (let i = 0, len = str.length; i < len; i += 1) {
+    byteArray.push(parseInt(str.substr(i, 2), 16));
+  }
+  return new Int8Array(byteArray); 
+}
